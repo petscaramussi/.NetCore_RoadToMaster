a527000b3c56e3bdca921608c2f3964731ef20ed
Peterson <44592787+petscaramussi@users.noreply.github.com>
2023-10-09 16:02:52 -0300

    add generics part

---
+++ Generics/GenericBubbleSortApp/GenericBubbleSortApp/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace GenericBubbleSortApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Object[] arr = new object[] { 1, 2, 3 };
+
+            SortArray sortArray = new SortArray();
+
+            sortArray.BubbleSort(arr);
+
+
+
+
+        }
+    }
+
+    public class SortArray
+    {
+        public void BubbleSort(object[] arr) 
+        {
+            int n = arr.Length;
+
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n - 1; j++)
+                {
+                    if (((IComparable)arr[j]).CompareTo(arr[j+1]) > 0) 
+                    {
+                        Swap(arr, j);
+                    }
+                }
+            }  
+        }
+
+        private void Swap(object[] arr, int j)
+        {
+            object tempo = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = tempo;
+        }
+    }
+}
