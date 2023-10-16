8d0d0d700b571caf87e9161d5a550cbea6c78677
Peterson <44592787+petscaramussi@users.noreply.github.com>
2023-10-16 11:54:02 -0300

    generic bubble end

--- Generics/GenericBubbleSortApp/GenericBubbleSortApp/Program.cs
+++ Generics/GenericBubbleSortApp/GenericBubbleSortApp/Program.cs
@@ -4,21 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Object[] arr = new object[] { 1, 2, 3 };
+            //int[] arr = new int[] { 2, 1, 4, 3 };
 
-            SortArray sortArray = new SortArray();
+            //string[] arr = new string[] { "bob", "Henry", "Andy", "Greg" };
+
+            Employee[] arr = new Employee[]
+            {
+                new Employee { Id = 4, Name = "John" },
+                new Employee { Id = 2, Name = "Bob" },
+                new Employee { Id = 3, Name = "Greg" },
+                new Employee { Id = 1, Name = "Tom" }
+            };
+
+            SortArray<Employee> sortArray = new SortArray<Employee>();
 
             sortArray.BubbleSort(arr);
 
+            foreach (var item in arr)
+                Console.WriteLine(item);
 
+            Console.ReadKey();
 
 
         }
     }
 
-    public class SortArray
+    public class Employee : IComparable<Employee>
     {
-        public void BubbleSort(object[] arr) 
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int CompareTo(Employee? other)
+        {
+            return this.Id.CompareTo(other.Id);
+        }
+
+        //public int CompareTo(object? obj)
+        //{
+        //    return this.Id.CompareTo(((Employee)obj).Id);
+        //}
+
+        public override string ToString()
+        {
+            return $"{Id} {Name}";
+        }
+    }
+
+    public class SortArray<T> where T:IComparable<T>
+    {
+        public void BubbleSort(T[] arr) 
         {
             int n = arr.Length;
 
@@ -26,7 +60,7 @@
             {
                 for(int j = 0; j < n - 1; j++)
                 {
-                    if (((IComparable)arr[j]).CompareTo(arr[j+1]) > 0) 
+                    if (arr[j].CompareTo(arr[j+1]) > 0) 
                     {
                         Swap(arr, j);
                     }
@@ -34,11 +68,11 @@
             }  
         }
 
-        private void Swap(object[] arr, int j)
+        private void Swap(T[] arr, int j)
         {
-            object tempo = arr[j];
+            T temp = arr[j];
             arr[j] = arr[j + 1];
-            arr[j + 1] = tempo;
+            arr[j + 1] = temp;
         }
     }
 }
