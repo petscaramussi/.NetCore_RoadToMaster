b7b227b7afa0ce4a14633be79458f29aacadb701
Peterson <peterson.silva6@fatec.sp.gov.br>
2024-06-03 14:16:29 -0300

    Delegates project 1

---
+++ Delegates/DelegateBasicExample/DelegateBasicExample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace DelegateBasicExample
+{
+    class Program
+    {
+        delegate void LogDel(string text);
+        static void Main(string[] args)
+        {
+            Log log = new Log();
+
+            LogDel LogTextToScreenDel, LogTextToFileDel;
+
+            LogTextToScreenDel = new LogDel(log.LogTextToScreen);
+            LogTextToFileDel = new LogDel(log.LogTextToFile);
+
+            LogDel multiLogDel = LogTextToScreenDel + LogTextToFileDel;
+
+            LogDel logDel = new LogDel(log.LogTextToScreen);
+
+            Console.WriteLine("Please enter your name");
+
+            var nome = Console.ReadLine();
+
+            multiLogDel(nome);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Log
+    {
+        public void LogTextToScreen(string text)
+        {
+            Console.WriteLine($"{DateTime.Now}: {text}");
+        }
+        public void LogTextToFile(string text)
+        {
+            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
+            {
+                sw.WriteLine($"{DateTime.Now}: {text}");
+            }
+        }
+    }
+}
+ 
