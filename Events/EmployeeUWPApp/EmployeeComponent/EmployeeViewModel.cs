c6b8b638a12b59e6fa616a8f357b081d6dd65fbe
Peterson <peterson.silva6@fatec.sp.gov.br>
2024-07-24 07:12:38 -0300

    Add new projects

---
+++ Events/EmployeeUWPApp/EmployeeComponent/EmployeeViewModel.cs
@@ -0,0 +1,43 @@
+﻿
+using System.ComponentModel;
+
+
+namespace EmployeeComponent
+{
+    public class EmployeeViewModel : INotifyPropertyChanged
+    {
+        private string _firstName = "";
+
+        public EmployeeViewModel()
+        {
+
+
+        }
+        public int Id { get; set; }
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                if (_firstName == value) return;
+                _firstName = value;
+                NotifyPropertyChanged("FirstName");
+            }
+        }
+        public string LastName { get; set; }
+        public decimal AnnualSalary { get; set; }
+        public char Gender { get; set; }
+        public bool IsManager { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+  
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ Events/EmployeeUWPApp/EmployeeUWPApp/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using EmployeeComponent;
+using System.Threading.Tasks;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace EmployeeUWPApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        ObservableCollection<EmployeeViewModel> _employeesOC = null;
+        public XamlUICommand ChangeFirstNameCommand = null;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Employees employees = new Employees();
+
+            _employeesOC = employees.GetEmployees();
+
+            EmployeesList.ItemsSource = _employeesOC;
+
+            ChangeFirstNameCommand = new XamlUICommand();
+            ChangeFirstNameCommand.ExecuteRequested += ChangeFirstNameCommand_ExecuteRequested;
+
+            btnThankYou.Click += BtnThankYou_Click;
+        }
+
+        private async void BtnThankYou_Click(object sender, RoutedEventArgs e)
+        {
+            await SpeakAsync("Thank you, and take care.");
+        }
+
+        private async void ChangeFirstNameCommand_ExecuteRequested(XamlUICommand sender, ExecuteRequestedEventArgs args)
+        {
+            ListView lv = (ListView)args.Parameter;
+            if (lv.SelectedIndex != -1)
+            {
+                await SpeakAsync($"Changing First name, from {_employeesOC[lv.SelectedIndex].FirstName}, to, {txtFirstName.Text}");
+                _employeesOC[lv.SelectedIndex].FirstName = txtFirstName.Text;
+            }
+
+        }
+        private async Task SpeakAsync(string text)
+        {
+
+            MediaElement mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync(text);
+            mediaElement.SetSource(stream, stream.ContentType);
+
+        }
+
+    }
+}
